18e94ce010cf11c0dac16046f775862b530372ea
DenizAtikkk <denizatikk@outlook.com>
2024-01-31 09:56:23 +0300

    Yusuf Deniz ATİK bitirme projesi
    Bitirme projesi olarak yaptığım giriş seviye bir sohbet uygulaması

---
+++ sohbet-uygulaması/src/functions/constants.tsx
@@ -0,0 +1,7 @@
+export const projectId = process.env.REACT_APP_PROJECT_ID
+  ? process.env.REACT_APP_PROJECT_ID
+  : "e34f81cb-94d9-4225-a9d9-4d7644db13ca";
+
+export const privateKey: string = process.env.REACT_APP_PROJECT_KEY
+  ? process.env.REACT_APP_PROJECT_KEY
+  : "fc0ac9c4-1e3d-4394-a4d1-2adb23d83d1a";
